a08d73ce2818724050baa9c5ee13a785cf603dd5
Laila Campbell <lailacampbell96@gmail.com>
2020-06-05 02:14:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-food = [red_meat, poultry, seafood, pork]
+food = [veggies, poultry, seafood, pork]
 end
 
 def add_element_to_end_of_array(array, element)
