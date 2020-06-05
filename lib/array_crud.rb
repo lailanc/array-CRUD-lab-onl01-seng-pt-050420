46939d7110290d0d1942899393f6209fe51d4307
Laila Campbell <lailacampbell96@gmail.com>
2020-06-05 02:47:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,11 +22,12 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+   
 end
 
 def retrieve_first_element_from_array(array)
